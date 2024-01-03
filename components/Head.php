75289fd8d292683d998dabc8ea374848c7b6e3cc
Esteban Zepeda <estzepeda@outlook.com>
2024-01-02 22:05:25 -0600

    Initial commit
    Started work in the index.php and some components (Head and Navbar)

---
+++ components/Head.php
@@ -0,0 +1,33 @@
+<head>
+    <!-- General -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="">
+    <meta name="author" content="Qarta Online">
+    <title>GISnet</title>
+    <meta name="description" content="">
+    <link rel="icon" href="assets/img/LogoBlack.svg">
+    <link rel="canonical" href="https://extra.qartaonline.com/gisnet">
+    <meta name="url" content="https://cocina113.com">
+    <link rel="icon" href="">
+    <link rel="apple-touch-icon" href="">
+    <meta name="msapplication-TileImage" content="">
+    <!-- Redes Sociales -->
+    <meta property="og:title" content="">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:image" content="">
+    <meta property="og:image:secure_url" content="">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="">
+    <meta name="twitter:card" content="summary_large_image">
+    <!-- Stylesheets -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="assets/css/fonts.css"> -->
+    <!-- <link rel="stylesheet" href="assets/css/keyframes.css"> -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- <link rel="stylesheet" href="assets/css/scrollAppearEffect.css"> -->
+</head>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+    <?php require("components/Head.php");?>
+	<body>
+		<nav></nav>
+		<header>
+			<h1>Gisnet</h1>
+		</header>
+		<main>
+			<section>
+            <?php
+                // Get the absolute path of the current file
+                $currentFilePath = __FILE__;
+
+                echo "The path to the current file is: " . $currentFilePath;
+            ?>
+            </section>
+		</main>
+		<footer></footer>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+	</body>
+</html>
