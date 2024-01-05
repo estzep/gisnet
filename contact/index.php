65de6997f11078332422b33051b09a3c4401946f
Esteban Zepeda <estzepeda@outlook.com>
2024-01-05 15:48:51 -0600

    Create index.php

---
+++ contact/index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="es">
+    <?php require("src/components/Head.php");?>
+	<body>
+        req Layout
+        <?php require("src/components/Layout.php");?>
+	</body>
+</html>
