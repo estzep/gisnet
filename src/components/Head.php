1aaf538ad96dabd6efa72b917413d9f51d31cede
Esteban Zepeda <estzepeda@outlook.com>
2024-01-08 14:53:57 -0600

    Imported global.css
    Imported global.css to Head component and deleted style.css

--- src/components/Head.php
+++ src/components/Head.php
@@ -35,8 +35,8 @@ if ($path == "") { $path = ''; }
     <!-- Stylesheets -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo($path);?>src/assets/css/fonts.css">
+    <link rel="stylesheet" href="<?php echo($path);?>src/assets/css/global.css">
     <!-- <link rel="stylesheet" href="assets/css/keyframes.css"> -->
-    <link rel="stylesheet" href="<?php echo($path);?>src/assets/css/style.css">
     <link rel="stylesheet" href="<?php echo($path);?>src/assets/css/navbar.css">
     <!-- <link rel="stylesheet" href="assets/css/scrollAppearEffect.css"> -->
 </head>
