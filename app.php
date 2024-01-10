f698a64cdd322741336184b9241ee5d25079100e
Esteban Zepeda <estzepeda@outlook.com>
2024-01-10 17:32:11 -0600

    Update

--- app.php
+++ app.php
@@ -28,3 +28,20 @@
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function() {
+        function mainPadding() {
+            var navbar = document.getElementsByTagName("nav")[0];
+            var main = document.getElementsByTagName("main")[0];
+            
+            var navbarHeight = navbar.offsetHeight;
+            main.style.paddingTop = navbarHeight + "px";
+        }
+        
+        mainPadding();
+        
+        window.addEventListener("resize", function() {
+            mainPadding();
+        });
+    });
+</script>
