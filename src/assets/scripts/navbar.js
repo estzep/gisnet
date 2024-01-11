71a46c5425cadaea42e9693137125a4b03485db1
Esteban Zepeda <estzepeda@outlook.com>
2024-01-10 20:50:14 -0600

    Update navbar.js

--- src/assets/scripts/navbar.js
+++ src/assets/scripts/navbar.js
@@ -1,8 +1,11 @@
 function toggleNav() {
+    var body = document.getElementsByTagName("body")[0];
     var navbar = document.getElementsByTagName("nav")[0];
     if (navbar.classList.contains("open")) {
+        body.classList.remove("overflow-y-hidden");
         navbar.classList.remove("open");
     } else {
+        body.classList.add("overflow-y-hidden");
         navbar.classList.add("open");
     }
 }
