c2d744c3074e5ac277cd6ab735b920b3621b1693
Esteban Zepeda <estzepeda@outlook.com>
2024-01-13 16:05:26 -0600

    Update

--- src/assets/scripts/navbar.js
+++ src/assets/scripts/navbar.js
@@ -13,18 +13,6 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-function toggleNav() {
-    var html = document.getElementsByTagName("html")[0];
-    var navbar = document.getElementsByTagName("nav")[0];
-    if (navbar.classList.contains("open")) {
-        html.classList.remove("overflow-y-hidden");
-        navbar.classList.remove("open");
-    } else {
-        html.classList.add("overflow-y-hidden");
-        navbar.classList.add("open");
-    }
-}
-
 function closeNav() {
     var html = document.getElementsByTagName("html")[0];
     var navbar = document.getElementsByTagName("nav")[0];
@@ -37,4 +25,13 @@ function openNav() {
     var navbar = document.getElementsByTagName("nav")[0];
     html.classList.add("overflow-y-hidden");
     navbar.classList.add("open");
+}
+
+function toggleNav() {
+    var navbar = document.getElementsByTagName("nav")[0];
+    if (navbar.classList.contains("open")) {
+        closeNav();
+    } else {
+        openNav();
+    }
 }
