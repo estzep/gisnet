f06899d7c3681fd92b16cf8d8d49bfb72993a765
Esteban Zepeda <estzepeda@outlook.com>
2024-01-14 18:05:26 -0600

    updae

--- src/assets/scripts/navbar.js
+++ src/assets/scripts/navbar.js
@@ -7,9 +7,11 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     
     mainPadding();
+    closeNav();
     
     window.addEventListener("resize", function() {
         mainPadding();
+        closeNav();
     });
 });
 
