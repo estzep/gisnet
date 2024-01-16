82d8a15d762dcdca2912dc5fd246ca72de3db621
Esteban Zepeda <estzepeda@outlook.com>
2024-01-15 23:28:11 -0600

    Update

--- src/pages/Home.php
+++ src/pages/Home.php
@@ -2,9 +2,9 @@
     <div class="container">
         <div class="content">
             <h1>Líderes en soluciones y consultorías especializadas</h1>
-            <h4 class="subtitle">
+            <p class="text-1">
                 20 años de experiencia innovando en el sector gubernamental y financiero
-            </h4>
+            </p>
             <div class="links">
                 <a href="contacto" class="btn-type-1 f-btn desktop">
                     Hablas con nosotros
@@ -25,7 +25,7 @@
 <section id="clientes">
     <div class="section-container container">
         <div class="content">
-            <h4>Con la confianza de</h4>
+            <h5>Con la confianza de</h5>
             <div class="slider">
                 <div class="overlay"></div>
                 <div class="track">
@@ -74,8 +74,34 @@
 <section id="empresa">
     <div class="section-container container">
         <div class="content">
-            <h2>Section 3</h2>
-            <h4>Empresa</h4>
+            <h2>Más de 22 años modernizando procesos de nuestros clientes</h2>
+            <p class="text-1">
+                Nos concentramos en la evolución tecnológica de nuestros clientes, mediante los servicios y productos que ofrecemos, dirigidos principalmente al sector municipal, estatal y federal.
+            </p>
+            <div class="squares">
+                <div class="square f-square">
+                    <div class="square-content">
+                        <h4>
+                            <img src="src/assets/icons/icon-1.svg" alt="Icon 1">
+                            Sector Gobierno
+                        </h4>
+                        <p class="text-2">
+                            Hemos participado activamente y con éxito en la Modernización integral de diversas áreas, incluyendo Registros Públicos de la Propiedad, Catastros Estatales, Registros Civiles, Salud, Vivienda (Infonavit) y Transportes (SCT).
+                        </p>
+                    </div>
+                </div>
+                <div class="square s-square">
+                    <div class="square-content">
+                        <h4>
+                            <img src="src/assets/icons/icon-2.svg" alt="Icon 2">
+                            Sector Financiero
+                        </h4>
+                        <p class="text-2">
+                            Damos instalación y mantenimiento a nivel nacional a los toma turnos de Banco BBVA y Santander, contando con un Call Center (Centro de llamado), ofreciendo soporte en la operación de los equipos instalados de más de 2,000 sucursales.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
