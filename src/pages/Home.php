24a1cafbff7048c1ecaaea25ed5d4304fffcbf47
Esteban Zepeda <estzepeda@outlook.com>
2024-01-15 19:34:44 -0600

    Update

--- src/pages/Home.php
+++ src/pages/Home.php
@@ -26,8 +26,27 @@
     <div class="section-container container">
         <div class="content">
             <h4>Con la confianza de</h4>
-            <div class="carousel">
-                carousel...
+            <div class="slider">
+                <div class="track">
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-bbva.png" alt="BBVA logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-cecoban.png" alt="Cecoban logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-diebold-nixdorf.png" alt="Diebold Nixdorf logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-gbo-gto.png" alt="Gobierno de Guanajuato logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-gbo-nl.png" alt="Goierno de Nuevo León logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-santander.png" alt="Santander logo">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
