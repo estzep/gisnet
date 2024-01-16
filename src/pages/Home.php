d119058f39ebd2f527479a2e07ba3949287f862f
Esteban Zepeda <estzepeda@outlook.com>
2024-01-15 20:37:52 -0600

    Update Home.php

--- src/pages/Home.php
+++ src/pages/Home.php
@@ -68,6 +68,48 @@
                     </div>
                 </div>
             </div>
+            <div class="slider">
+                <div class="overlay"></div>
+                <div class="track">
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-bbva.png" alt="BBVA logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-cecoban.png" alt="Cecoban logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-diebold-nixdorf.png" alt="Diebold Nixdorf logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-gbo-gto.png" alt="Gobierno de Guanajuato logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-gbo-nl.png" alt="Goierno de Nuevo León logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-santander.png" alt="Santander logo">
+                    </div>
+                    <!-- repeat -->
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-bbva.png" alt="BBVA logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-cecoban.png" alt="Cecoban logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-diebold-nixdorf.png" alt="Diebold Nixdorf logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-gbo-gto.png" alt="Gobierno de Guanajuato logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-gbo-nl.png" alt="Goierno de Nuevo León logo">
+                    </div>
+                    <div class="item">
+                        <img src="<? echo($path) ?>src/assets/img/logos/logo-santander.png" alt="Santander logo">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
