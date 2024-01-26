06537663c6e1dc4f57105939ed3f5aa4986a8718
Esteban Zepeda <estzepeda@outlook.com>
2024-01-26 15:18:01 -0600

    Update Home.php

--- src/pages/Home.php
+++ src/pages/Home.php
@@ -171,11 +171,10 @@
 
             <div class="d-flex align-items-start">
                 <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                    <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</button>
-                    <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</button>
-                    <button class="nav-link" id="v-pills-disabled-tab" data-bs-toggle="pill" data-bs-target="#v-pills-disabled" type="button" role="tab" aria-controls="v-pills-disabled" aria-selected="false" disabled>Disabled</button>
-                    <button class="nav-link" id="v-pills-messages-tab" data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</button>
-                    <button class="nav-link" id="v-pills-settings-tab" data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</button>
+                    <button class="nav-link active" id="li-servicio1" data-bs-toggle="pill" data-bs-target="#tab-servicio1" type="button" role="tab" aria-controls="tab-servicio1" aria-selected="true">Home</button>
+                    <button class="nav-link" id="li-servicio2" data-bs-toggle="pill" data-bs-target="#tab-servicio2" type="button" role="tab" aria-controls="tab-servicio2" aria-selected="false">Profile</button>
+                    <button class="nav-link" id="3-servicio3" data-bs-toggle="pill" data-bs-target="#tab-servicio3" type="button" role="tab" aria-controls="tab-servicio3" aria-selected="false">Disabled</button>
+                    <button class="nav-link" id="tab-servicio4" data-bs-toggle="pill" data-bs-target="#tab-servicio4" type="button" role="tab" aria-controls="tab-servicio4" aria-selected="false">Settings</button>
                 </div>
                 <div class="tab-content" id="v-pills-tabContent">
                     <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab" tabindex="0">...</div>
