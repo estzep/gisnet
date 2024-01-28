e0d88a7c9d8d7c3d89aa18515a60826fe28bdeae
Esteban Zepeda <estzepeda@outlook.com>
2024-01-28 12:41:02 -0600

    Update

--- src/pages/Error.php
+++ src/pages/Error.php
@@ -1,37 +1,36 @@
 <?php
-    $errorCode = http_response_code();
+    // $errorCode = http_response_code();
 
-    $errorMessages = [
-        404 => [
-            'title' => '404',
-            'message' => 'Lo sentimos, la página no fue encontrada.'
-        ],
-        403 => [
-            'title' => '403',
-            'message' => 'Accesso no autorizado.'
-        ],
-    ];
-    if (array_key_exists($errorCode, $errorMessages)) {
-        $error = $errorMessages[$errorCode];
-    } else {
-        $error = [
-            'title' => 'Error',
-            'message' => 'Ocurrió un error inesperado.'
-        ];
-    }
+    // $errorMessages = [
+    //     404 => [
+    //         'title' => '404',
+    //         'message' => 'Lo sentimos, la página no fue encontrada.'
+    //     ],
+    //     403 => [
+    //         'title' => '403',
+    //         'message' => 'Accesso no autorizado.'
+    //     ],
+    // ];
+    // if (array_key_exists($errorCode, $errorMessages)) {
+    //     $error = $errorMessages[$errorCode];
+    // } else {
+    //     $error = [
+    //         'title' => 'Error',
+    //         'message' => 'Ocurrió un error inesperado.'
+    //     ];
+    // }
 
     $base = substr($_SERVER['REQUEST_URI'], strlen('/gisnet'));
     $segments = count(array_filter(explode('/', $base)));
     $path = str_repeat('../', $segments);
 ?>
 
-<header id="inicio">
+<header id="inicio" class="error">
     <div class="container">
         <div class="content">
-            <p><?php echo $errorCode; ?></p>
-            <h1><?php echo $error['title']; ?></h1>
+            <h1>404</h1>
             <p class="text-1">
-                <?php echo $error['message']; ?>
+                Lo sentimos, la página no fue encontrada.
             </p>
             <div class="links">
                 <a href="<? echo($path) ?>/" class="btn-type-1 f-btn">
