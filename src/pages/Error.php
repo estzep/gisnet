276b07561d690d99cb83c12e1eab1e32f083869f
Esteban Zepeda <estzepeda@outlook.com>
2024-01-28 11:51:31 -0600

    Update Error.php

--- src/pages/Error.php
+++ src/pages/Error.php
@@ -1,4 +1,25 @@
 <?php
+    $errorCode = http_response_code();
+
+    $errorMessages = [
+        404 => [
+            'title' => '404',
+            'message' => 'Lo sentimos, la página no fue encontrada.'
+        ],
+        403 => [
+            'title' => '403',
+            'message' => 'Accesso no autorizado.'
+        ],
+    ];
+    if (array_key_exists($errorCode, $errorMessages)) {
+        $error = $errorMessages[$errorCode];
+    } else {
+        $error = [
+            'title' => 'Error',
+            'message' => 'Ocurrió un error inesperado.'
+        ];
+    }
+
     $base = substr($_SERVER['REQUEST_URI'], strlen('/gisnet'));
     $segments = count(array_filter(explode('/', $base)));
     $path = str_repeat('../', $segments);
@@ -7,12 +28,13 @@
 <header id="inicio">
     <div class="container">
         <div class="content">
-            <h1>404</h1>
+            <p><?php echo $errorCode; ?></p>
+            <h1><?php echo $error['title']; ?></h1>
             <p class="text-1">
-                Lo sentimos, la página no fue encontrada.
+                <?php echo $error['message']; ?>
             </p>
             <div class="links">
-                <a href="<? echo($path) ?>" class="btn-type-1 f-btn">
+                <a href="<? echo($path) ?>/" class="btn-type-1 f-btn">
                     Regresar a inicio
                 </a>
             </div>
