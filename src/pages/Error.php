188b384afa15053eb0c60467a1e741112a2d206e
Esteban Zepeda <estzepeda@outlook.com>
2024-01-28 11:44:17 -0600

    Update Error.php

--- src/pages/Error.php
+++ src/pages/Error.php
@@ -1,3 +1,21 @@
-<header>
-    error
+<?php
+    $base = substr($_SERVER['REQUEST_URI'], strlen('/gisnet'));
+    $segments = count(array_filter(explode('/', $base)));
+    $path = str_repeat('../', $segments);
+?>
+
+<header id="inicio">
+    <div class="container">
+        <div class="content">
+            <h1>404</h1>
+            <p class="text-1">
+                Lo sentimos, la página no fue encontrada.
+            </p>
+            <div class="links">
+                <a href="<? echo($path) ?>" class="btn-type-1 f-btn">
+                    Regresar a inicio
+                </a>
+            </div>
+        </div>
+    </div>
 </header>
