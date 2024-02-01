b69fe3f4ffaf25262851c55a49bbe978d55e1271
Esteban Zepeda <estzepeda@outlook.com>
2024-02-01 11:03:28 -0600

    Update

---
+++ src/components/Header.php
@@ -0,0 +1,23 @@
+<?php
+    $title = "Testing test";
+    $description = "This is a testing description.";
+?>
+
+<header>
+    <div class="container">
+        <div class="content">
+            <div class="info">
+                <h1><? echo $title ?></h1>
+                <p class="text-1">
+                    <? echo $description ?>
+                </p>
+                <a href="<? echo $path ?>/contacto" class="btn-type-1 f-btn">
+                    Quiero comenzar
+                </a>
+            </div>
+            <div class="image">
+                <img src="<? echo $path ?>" alt="Producto / Servicio image">
+            </div>
+        </div>
+    </div>
+</header>
--- src/pages/ServMigracion.php
+++ src/pages/ServMigracion.php
@@ -1,3 +1,11 @@
-<header>
-    migracion
-</header>
+<?php
+    $page = substr($_SERVER['REQUEST_URI'], strlen('/gisnet'));
+    include($path.'src/components/Header.php');
+?>
+<section>
+    <div class="container">
+        <div class="content">
+            migracion de acervos
+        </div>
+    </div>
+</section>
