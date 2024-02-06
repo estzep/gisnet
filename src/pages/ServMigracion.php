a5fe4cb69fd8ec97f325ddc2998a1db6a77b212c
Esteban Zepeda <estzepeda@outlook.com>
2024-02-06 13:33:59 -0600

    Update ServMigracion.php

--- src/pages/ServMigracion.php
+++ src/pages/ServMigracion.php
@@ -2,6 +2,15 @@
     $base = substr($_SERVER['REQUEST_URI'], strlen('/gisnet'));
     $segments = count(array_filter(explode('/', $base)));
     $path = str_repeat('../', $segments);
+    
+    $data = json_decode($jsonString);$jsonString = file_get_contents($path.'src/assets/data/services.json');
+    $data = json_decode($jsonString, true);
+    
+    if ($data === null) {
+        echo 'Error decoding JSON: ' . json_last_error_msg();
+    } else {
+        echo $data->key;
+    }
 
     $headerTitle = "Migración de acervos";
     $headerDescription = "El proceso de transferir datos, documentos, imágenes y registros de un formato o sistema de almacenamiento a otro, más moderno y accesible.";
