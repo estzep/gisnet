ec0b2b59fd43cb8f23e5eeeafd3ffed9b627915a
Esteban Zepeda <estzepeda@outlook.com>
2024-02-09 17:47:26 -0600

    Update

--- src/components/Header.php
+++ src/components/Header.php
@@ -17,7 +17,7 @@
                 </a>
             </div>
             <div class="image">
-                <img src="<? echo $path ?>src/assets/img/sp/<? echo $spname ?>/<? echo $headerImg ?>" alt="Producto/Servicio image">
+                <img src="<? echo $path ?>src/assets/img/sp/<? echo $headerImg ?>" alt="Producto/Servicio image">
             </div>
         </div>
     </div>
