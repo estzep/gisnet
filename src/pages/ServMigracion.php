19da91ddd263c0547422dace0bfb871128a90a2d
Esteban Zepeda <estzepeda@outlook.com>
2024-08-09 16:13:18 -0600

    Update ServMigracion.php

--- src/pages/ServMigracion.php
+++ src/pages/ServMigracion.php
@@ -55,7 +55,7 @@
             <div class="tabs">
                 <div class="column info">
                     <ul class="tablist" role="tablist">
-                        <li id="li-tab1" class="tab active" aria-selected="true">
+                        <li id="li-tab1" class="tab active" data-bs-toggle="tab" data-bs-target="#tab-tab1" role="tab" aria-controls="tab-tab1" aria-selected="true">
                             <div class="tab-content">
                                 <div class="heading">
                                     <h6>Inventario General</h6>
@@ -75,7 +75,7 @@
                                 </div>
                             </div>
                         </li>
-                        <li id="li-tab2" class="tab" aria-selected="false">
+                        <li id="li-tab2" class="tab" data-bs-toggle="tab" data-bs-target="#tab-tab2" role="tab" aria-controls="tab-tab2" aria-selected="false">
                             <div class="tab-content">
                                 <div class="heading">
                                     <h6>Depuración y revisión del acervo documental</h6>
@@ -95,7 +95,7 @@
                                 </div>
                             </div>
                         </li>
-                        <li id="li-tab3" class="tab" aria-selected="false">
+                        <li id="li-tab3" class="tab" data-bs-toggle="tab" data-bs-target="#tab-tab3" role="tab" aria-controls="tab-tab3" aria-selected="false">
                             <div class="tab-content">
                                 <div class="heading">
                                     <h6>Digitalización</h6>
@@ -115,7 +115,7 @@
                                 </div>
                             </div>
                         </li>
-                        <li id="li-tab4" class="tab" aria-selected="false">
+                        <li id="li-tab4" class="tab" data-bs-toggle="tab" data-bs-target="#tab-tab4" role="tab" aria-controls="tab-tab4" aria-selected="false">
                             <div class="tab-content">
                                 <div class="heading">
                                     <h6>Control de calidad en todo el proceso</h6>
@@ -138,16 +138,7 @@
                     </ul>
                 </div>
                 <div class="column image tab-content">
-                    <div id="tab-tab1" class="panel tab-pane fade show active" role="tabpanel" aria-labelledby="li-tab1" tabindex="0">
-                        <img src="<?php echo($path); ?>src/assets/img/placeholder.png" alt="Placeholder image">
-                    </div>
-                    <div id="tab-tab2" class="panel tab-pane fade" role="tabpanel" aria-labelledby="li-tab2" tabindex="0">
-                        <img src="<?php echo($path); ?>src/assets/img/placeholder.png" alt="Placeholder image">
-                    </div>
-                    <div id="tab-tab3" class="panel tab-pane fade" role="tabpanel" aria-labelledby="li-tab3" tabindex="0">
-                        <img src="<?php echo($path); ?>src/assets/img/placeholder.png" alt="Placeholder image">
-                    </div>
-                    <div id="tab-tab4" class="panel tab-pane fade" role="tabpanel" aria-labelledby="li-tab4" tabindex="0">
+                    <div class="panel tab-pane fade show active">
                         <img src="<?php echo($path); ?>src/assets/img/placeholder.png" alt="Placeholder image">
                     </div>
                 </div>
