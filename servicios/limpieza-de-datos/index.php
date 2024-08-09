66a6acbc1b6931cfc4fa7bcd301ebf47b6df6ba1
Esteban Zepeda <estzepeda@outlook.com>
2024-08-09 17:05:49 -0600

    asd

--- servicios/limpieza-de-datos/index.php
+++ servicios/limpieza-de-datos/index.php
@@ -4,8 +4,11 @@
     $url = 'https://qartaonline.com/gisnet';
 
     $base = substr($_SERVER['REQUEST_URI'], strlen('/gisnet'));
+    echo "base: " . $base;
     $segments = count(array_filter(explode('/', $base)));
+    echo "segments: " . $segments;
     $path = str_repeat('../', $segments);
+    echo "path: " . $path;
 ?>
 
 <!DOCTYPE html>
