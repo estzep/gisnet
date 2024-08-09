693267592af24343d0c94743b85a393f2ff5c836
Esteban Zepeda <estzepeda@outlook.com>
2024-08-09 13:26:35 -0600

    Update ServLimpieza.php

--- src/pages/ServLimpieza.php
+++ src/pages/ServLimpieza.php
@@ -64,19 +64,19 @@
         <div class="content mixed-content-1">
             <div class="info">
                 <h3>Resultados de calidad</h3>
-                <h4>Precisión Mejorada</h4>
+                <h6>Precisión Mejorada</h6>
                 <p class="text-3">
                     La base de datos migrada y limpia cuenta con una precisión significativamente mayor, eliminando errores e inexactitudes.
                 </p>
-                <h4>Consistencia</h4>
+                <h6>Consistencia</h6>
                 <p class="text-3">
                     Los datos se estandarizan siguiendo un formato uniforme, lo que facilita el análisis y la integración con otros sistemas.
                 </p>
-                <h4>Optimización de Rendimiento</h4>
+                <h6>Optimización de Rendimiento</h6>
                 <p class="text-3">
                     La base de datos presenta un rendimiento óptimo, con tiempos de respuesta más rápidos y una gestión más eficiente.
                 </p>
-                <h4>Mayor Seguridad</h4>
+                <h6>Mayor Seguridad</h6>
                 <p class="text-3">
                     Garantizando que la información sensible esté protegida y la base de datos sea más confiable y segura para su uso.
                 </p>
