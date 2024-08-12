18357a755f8d7a0b0453b7c54303aefa2044ed92
Esteban Zepeda <estzepeda@outlook.com>
2024-08-12 17:26:27 -0600

    update

--- src/pages/ProdTassta.php
+++ src/pages/ProdTassta.php
@@ -23,8 +23,113 @@
 <? include($path.'src/components/Clients.php'); ?>
 <section>
     <div class="section-container container">
+        <div class="content mixed-content-1">
+            <div class="info">
+                <h3>Obtén información rápida y relevante sobre el inventario de tus dispositivos</h3>
+                <p class="text-3">
+                    Ofrece una solución moderna Push-to-Talk que utiliza todas las capacidades técnicas de uso de smartphones, a través de la RED LTE (SIM de TELCEL, MOVISTAR, ATT, ALTAN-9.
+                    <br>
+                    Mediante el hardware estándar, cada organización puede prepararse tener un Sistema completo e integral de Comunicación.
+                </p>
+            </div>
+            <div class="image f contain">
+                <img src="<? echo $path ?>src/assets/img/tassta-img/img-2.jpg" alt="Producto/Servicio image">
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container section-container">
         <div class="content">
-            Contenido...
+            <h3 class="text-center mb-5">Componentes Principales</h3>
+            <div class="g-cards-1">
+                <div class="g-card tassta">
+                    <div class="top">
+                        <div class="icon">
+                            <img src="<? echo $path ?>src/assets/img/tassta-img/rodon.jpg" alt="Icon">
+                        </div>
+                        <h6>T.Rodon</h6>
+                        <p class="text-4">
+                            Solución profesional de comando y control
+                        </p>
+                    </div>
+                    <div class="bottom">
+                        <a href="<? echo($path) ?>" class="link btn-type-2">
+                            Ver PDF
+                            <span class="arrow">
+                                <img src="<? echo($path) ?>src/assets/icons/icon-arrow-right.svg" alt="Ir a" class="head">
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="g-card tassta">
+                    <div class="top">
+                        <div class="icon">
+                            <img src="<? echo $path ?>src/assets/img/tassta-img/flex.jpg" alt="Icon">
+                        </div>
+                        <h6>Escáner de Cheques</h6>
+                        <p class="text-4">
+                            Solución profesional push-to-talk (PTT) de banda ancha
+                        </p>
+                    </div>
+                    <div class="bottom">
+                        <a href="<? echo($path) ?>" class="link btn-type-2">
+                            Ver PDF
+                            <span class="arrow">
+                                <img src="<? echo($path) ?>src/assets/icons/icon-arrow-right.svg" alt="Ir a" class="head">
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="g-card tassta">
+                    <div class="top">
+                        <div class="icon">
+                            <img src="<? echo $path ?>src/assets/img/tassta-img/lion.jpg" alt="Icon">
+                        </div>
+                        <h6>Innovación para los bancos</h6>
+                        <p class="text-4">
+                            Servidor de nube y standalone
+                        </p>
+                    </div>
+                    <div class="bottom">
+                        <a href="<? echo($path) ?>" class="link btn-type-2">
+                            Ver PDF
+                            <span class="arrow">
+                                <img src="<? echo($path) ?>src/assets/icons/icon-arrow-right.svg" alt="Ir a" class="head">
+                            </span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container section-container">
+        <div class="content">
+            <h3>Características</h3>
+            <div class="g-cards-3"></div>
+        </div>
+    </div>
+</section>
+<section class="badges">
+    <div class="section-container container">
+        <div class="content">
+            <h3 class="text-center mb-5">Seguridad, certificados y cumplimientos</h3>
+            <div class="badges-container">
+                <div class="badge">
+                    <img src="<? echo($path) ?>src/assets/img/tassta-img/3pp.png" alt="Badge">
+                </div>
+                <div class="badge">
+                    <img src="<? echo($path) ?>src/assets/img/tassta-img/etsi.png" alt="Badge">
+                </div>
+                <div class="badge">
+                    <img src="<? echo($path) ?>src/assets/img/tassta-img/firstnet.png" alt="Badge">
+                </div>
+                <div class="badge">
+                    <img src="<? echo($path) ?>src/assets/img/tassta-img/gs.png" alt="Badge">
+                </div>
+            </div>
         </div>
     </div>
 </section>
