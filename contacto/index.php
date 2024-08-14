c409128c691e711136827282727b618b586e7e2b
Esteban Zepeda <estzepeda@outlook.com>
2024-08-13 19:07:02 -0600

    Update index.php

--- contacto/index.php
+++ contacto/index.php
@@ -13,6 +13,8 @@
 <html lang="es">
     <?php require($path."src/components/Head.php");?>
 	<body>
+        <? $page = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        echo($page); ?>
         <?php require($path."app.php");?>
 	</body>
 </html>
