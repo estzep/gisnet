67de7f7ee34c0a2dbef82aabc15312ea407fbb14
Esteban Zepeda <estzepeda@outlook.com>
2024-10-07 16:06:27 -0600

    Update Head.php

--- src/components/Head.php
+++ src/components/Head.php
@@ -33,8 +33,6 @@
     
     <!-- Favicon and icons -->
     <link rel="icon" href="<?php echo($path);?>favicon.ico" sizes="256x256" type="image/x-icon">
-    <link rel="icon" href="<?php echo($path);?>src/assets/gisnet/gisnet-180x180.png" type="image/png" sizes="180x180">
-    <link rel="icon" href="<?php echo($path);?>src/assets/gisnet/gisnet.png" type="image/png" sizes="180x45">
     <link rel="apple-touch-icon" href="<?php echo($path);?>src/assets/gisnet/gisnet-180x180.png">
     <meta name="msapplication-TileImage" content="<?php echo($path);?>src/assets/gisnet/gisnet-180x180.png">
     
