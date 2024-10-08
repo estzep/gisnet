cc950c90a81fe6989bb213c383058086e9721919
Esteban Zepeda <estzepeda@outlook.com>
2024-10-08 12:48:02 -0600

    Update CustomIcons.php

--- src/components/CustomIcons.php
+++ src/components/CustomIcons.php
@@ -3,6 +3,11 @@ $icons = [
     ['src' => 'src/assets/img/hero/custom/square-2.svg', 'alt' => 'Square'],
     ['src' => 'src/assets/img/hero/custom/arrows.svg', 'alt' => 'Arrows'],
     ['src' => 'src/assets/img/hero/custom/square-1.svg', 'alt' => 'Square'],
+    ['src' => 'src/assets/img/hero/custom/database.svg', 'alt' => 'Database'],
+    ['src' => 'src/assets/img/hero/custom/gear.svg', 'alt' => 'Gear'],
+    ['src' => 'src/assets/img/hero/custom/interface.svg', 'alt' => 'Interface'],
+    ['src' => 'src/assets/img/hero/custom/circle.svg', 'alt' => 'Circle'],
+    ['src' => 'src/assets/img/hero/custom/user.svg', 'alt' => 'User'],
 ];
 ?>
 <div class="custom-icons">
