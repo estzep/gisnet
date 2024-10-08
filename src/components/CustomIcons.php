b4af6b2f6b9662e2f2cc5b6b5f3ca9b31a6b424a
Esteban Zepeda <estzepeda@outlook.com>
2024-10-08 12:44:54 -0600

    Upd

--- src/components/CustomIcons.php
+++ src/components/CustomIcons.php
@@ -1,3 +1,18 @@
+<?php
+$icons = [
+    ['src' => 'src/assets/img/hero/custom/square-2.svg', 'alt' => 'Square'],
+    ['src' => 'src/assets/img/hero/custom/arrows.svg', 'alt' => 'Arrows'],
+    ['src' => 'src/assets/img/hero/custom/square-1.svg', 'alt' => 'Square'],
+];
+?>
 <div class="custom-icons">
-    hola
+    <div class="container">
+        <div class="content">
+            <?php foreach ($icons as $icon): ?>
+                <div class="icon">
+                    <img src="<?php echo $icon['src']; ?>" alt="<?php echo $icon['alt']; ?>">
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </div>
