93ea61cd007fd22014c1c09ef2f070ab0d111195
Esteban Zepeda <estzepeda@outlook.com>
2024-10-16 12:17:04 -0600

    Update CustomIcons.php

--- src/components/CustomIcons.php
+++ src/components/CustomIcons.php
@@ -1,35 +1,35 @@
 <div class="custom-icons">
     <div class="bg-img">
-        <img src="src/assets/img/hero/custom/bg.svg" alt="Background">
+        <img src="<? echo($path) ?>src/assets/img/hero/custom/bg.svg" alt="Background">
     </div>
     <div class="container">
         <div class="grid">
             <div class="icon blank"></div>
             <div class="icon square">
-                <img src="src/assets/img/hero/custom/square-2.svg" alt="Square">
+                <img src="<? echo($path) ?>src/assets/img/hero/custom/square-2.svg" alt="Square">
             </div>
             <div class="icon arrows">
-                <img src="src/assets/img/hero/custom/arrows.svg" alt="Arrows">
+                <img src="<? echo($path) ?>src/assets/img/hero/custom/arrows.svg" alt="Arrows">
             </div>
             <div class="icon blank"></div>
             <div class="icon square">
-                <img src="src/assets/img/hero/custom/square-1.svg" alt="Square">
+                <img src="<? echo($path) ?>src/assets/img/hero/custom/square-1.svg" alt="Square">
             </div>
             <div class="icon database">
-                <img src="src/assets/img/hero/custom/database.svg" alt="Database">
+                <img src="<? echo($path) ?>src/assets/img/hero/custom/database.svg" alt="Database">
             </div>
             <div class="icon gear">
-                <img src="src/assets/img/hero/custom/gear.svg" alt="Gear">
+                <img src="<? echo($path) ?>src/assets/img/hero/custom/gear.svg" alt="Gear">
             </div>
             <div class="icon interface">
-                <img src="src/assets/img/hero/custom/interface.svg" alt="Interface">
+                <img src="<? echo($path) ?>src/assets/img/hero/custom/interface.svg" alt="Interface">
             </div>
             <div class="icon blank"></div>
             <div class="icon circle">
-                <img src="src/assets/img/hero/custom/circle.svg" alt="Circle">
+                <img src="<? echo($path) ?>src/assets/img/hero/custom/circle.svg" alt="Circle">
             </div>
             <div class="icon user">
-                <img src="src/assets/img/hero/custom/user.svg" alt="User">
+                <img src="<? echo($path) ?>src/assets/img/hero/custom/user.svg" alt="User">
             </div>
             <div class="icon blank"></div>
         </div>
