c5ffa4bdf25ec275a163dccd886dcb0a8fed70ef
Esteban Zepeda <estzepeda@outlook.com>
2024-10-18 14:37:23 -0600

    Update Contact.php

--- src/pages/Contact.php
+++ src/pages/Contact.php
@@ -40,7 +40,7 @@ $productSelected = (in_array($asunto, array_column($productsData['products'], 's
                         <span id="emailError" class="error-msg">Este campo es obligatorio</span>
                     </div>
                     <div class="form-column full">
-                        <select name="reason" id="reasonSelect" class="<?php echo ($spSelected) ? 'disabled' : ''; ?>" required onchange="handleSelects()">
+                        <select name="reason" id="reasonSelect" class="<?php echo ($spSelected) ? 'disabled' : ''; ?>" required onchange="handleSelects(), validateInput(this)">
                             <option value="0" selected disabled>* Elegir asunto</option>
                             <option value="1" <?php echo ($serviceSelected) ? 'selected' : ''; ?>>Consulta sobre servicio</option>
                             <option value="2" <?php echo ($productSelected) ? 'selected' : ''; ?>>Consulta sobre producto</option>
@@ -51,7 +51,7 @@ $productSelected = (in_array($asunto, array_column($productsData['products'], 's
                         <span id="reasonError" class="error-msg">Este campo es obligatorio</span>
                     </div>
                     <div id="service" class="form-column full <?php echo ($serviceSelected) ? '' : 'hidden'; ?>">
-                        <select name="service" id="serviceSelect" class="<?php echo ($serviceSelected) ? '' : 'disabled'; ?>" onchange="handleSelects()">
+                        <select name="service" id="serviceSelect" class="<?php echo ($serviceSelected) ? '' : 'disabled'; ?>" onchange="handleSelects(), validateInput(this)">
                             <option value="0" <?php echo ($serviceSelected) ? '' : 'selected'; ?> disabled>* Seleccionar servicio</option>
                             <?php
                             foreach ($servicesData['services'] as $service) {
@@ -62,7 +62,7 @@ $productSelected = (in_array($asunto, array_column($productsData['products'], 's
                         <span id="serviceError" class="error-msg">Este campo es obligatorio</span>
                     </div>
                     <div id="product" class="form-column full <?php echo ($productSelected) ? '' : 'hidden'; ?>">
-                        <select name="product" id="productSelect" class="<?php echo ($productSelected) ? '' : 'disabled'; ?>" onchange="handleSelects()">
+                        <select name="product" id="productSelect" class="<?php echo ($productSelected) ? '' : 'disabled'; ?>" onchange="handleSelects(), validateInput(this)">
                             <option value="0" <?php echo ($productSelected) ? '' : 'selected'; ?> disabled>* Seleccionar producto</option>
                             <?php
                             foreach ($productsData['products'] as $product) {
