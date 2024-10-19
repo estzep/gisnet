93768f5554cb3a70108fb53bfa3ea51499eb004b
Esteban Zepeda <estzepeda@outlook.com>
2024-10-19 14:56:43 -0600

    Update contact.php

--- src/assets/scripts/contact.php
+++ src/assets/scripts/contact.php
@@ -1,18 +1,53 @@
 <?php
-if (isset($_POST['submit'])) {
-	$fname = $_POST['fname'];
-	$lname = $_POST['lname'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$fname = filter_input(INPUT_POST, 'fname', FILTER_UNSAFE_RAW);
+	$lname = filter_input(INPUT_POST, 'lname', FILTER_UNSAFE_RAW);
+	$company = filter_input(INPUT_POST, 'company', FILTER_UNSAFE_RAW);
+	$tel = filter_input(INPUT_POST, 'tel', FILTER_UNSAFE_RAW);
+	$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+	$reason = filter_input(INPUT_POST, 'reason', FILTER_UNSAFE_RAW);
+	if ($reason == 1) {
+		$service = filter_input(INPUT_POST, 'service', FILTER_UNSAFE_RAW);
+	} else if ($reason == 2) {
+		$product = filter_input(INPUT_POST, 'product', FILTER_UNSAFE_RAW);
+	}
+	$message = filter_input(INPUT_POST, 'message', FILTER_UNSAFE_RAW);
+
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		header("Location: ../../Contact.php?mail=invalid_email");
+		exit();
+	}
+
 	$name = $fname . ' ' . $lname;
-	$tel = $_POST['tel'];
-	$mailFrom = $_POST['email'];
-	$subject = $_POST['reason'];
-	$message = $_POST['message'];
+	$mailTo = "die-tae@hotmail.com";
+	$subject = "Nuevo contacto: " . $reason;
+	
+	$txt = "Nuevo correo de: $name\n";
+	$txt .= "Correo: $email\n";
+	$txt .= "Teléfono: $tel\n";
+	$txt .= "Compañía: $company\n";
+	$txt .= "Asunto: $reason\n";
+	if ($service) {
+		$txt .= "Servicio: $service\n";
+	}
+	if ($product) {
+		$txt .= "Producto: $product\n";
+	}
+	$txt .= "\nMensaje:\n$message";
 
-	$mailTo = "bacanoraterraza@gmail.mx";
-	$headers = "From: ".$mailFrom;
-	$txt = "Nuevo correo de ".$name."\nCorreo: ".$mailFrom."\nTelefono: ".$tel."\n"."Asunto: ".$subject."\n\n".$message;
+	$headers = array(
+		'From' => $email,
+		'Reply-To' => $email,
+		'X-Mailer' => 'PHP/' . phpversion()
+	);
 
-	mail($mailTo, $subject, $txt, $headers);
-	header("Location: index.php?mail=sent");
+	if (mail($mailTo, $subject, $txt, $headers)) {
+		header("Location: ../../Contact.php?mail=sent");
+	} else {
+		header("Location: ../../Contact.php?mail=error");
+	}
+} else {
+	header("Location: ../../Contact.php?mail=error");
 }
+exit();
 ?>
