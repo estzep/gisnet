151deda5e6e7b782a26f9abbee1243cc5dc9865b
Esteban Zepeda <estzepeda@outlook.com>
2024-12-19 18:36:53 -0600

    Update contact.php

--- src/assets/scripts/contact.php
+++ src/assets/scripts/contact.php
@@ -96,7 +96,17 @@ try {
 				'From' => $email,
 				'X-Mailer' => 'PHP/' . phpversion()
 			),
-			'message' => "Nuevo correo de: $name\n"
+			'message' => '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    hola
+</body>
+</html>'
 		];
 
 		// $userMail = "";
